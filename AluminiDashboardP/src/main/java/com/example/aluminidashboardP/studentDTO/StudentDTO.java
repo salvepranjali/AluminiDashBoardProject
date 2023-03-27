356618aef83a45522ddfabbcabd06680030fcd3d
salvepranjali <101197017+salvepranjali@users.noreply.github.com>
2023-03-27 04:18:17 -0400

    Update StudentDTO.java

--- AluminiDashboardP/src/main/java/com/example/aluminidashboardP/studentDTO/StudentDTO.java
+++ AluminiDashboardP/src/main/java/com/example/aluminidashboardP/studentDTO/StudentDTO.java
@@ -14,21 +14,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
+//DTO class use for the validation
 public class StudentDTO {
+	//validation for Student name
 	@NotBlank(message="Invalid name=Empty Name")
 	@NotNull(message="Invalid Name=Name is Empty")
 	@Size(min=3,max=30,message="Invalid Name:Must Be of 0-30 character")
 	private String name;
-	
+	//validation for Student Phone Number
 	@NotBlank(message="Invalid Phone Number=Empty Number")
 	@NotNull(message="Invalid Phone Number=Number is Empty")
 	@Pattern(regexp="\\d(10)$",message="Invalid Phone Number.........")
 	private String phone;
-	
+	//validation for Student Email
 	@NotNull
 	@Email(message="Invalid Email")
 	private String email; 
-	
+	//validation for Student Graduation Year
 	@NotBlank(message="Invalid name=Empty name")
 	@NotNull(message="Invalid name=name is Empty")
 	private String grdYear;
