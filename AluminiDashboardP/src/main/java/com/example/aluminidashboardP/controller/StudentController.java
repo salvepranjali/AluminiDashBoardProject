eb0594063f05492a3762a5b8fb3c65f7f3535819
salvepranjali <101197017+salvepranjali@users.noreply.github.com>
2023-03-27 03:45:18 -0400

    Update StudentController.java

--- AluminiDashboardP/src/main/java/com/example/aluminidashboardP/controller/StudentController.java
+++ AluminiDashboardP/src/main/java/com/example/aluminidashboardP/controller/StudentController.java
@@ -22,25 +22,28 @@ import com.example.aluminidashboardP.studentDTO.StudentDTO;
 
 import jakarta.validation.Valid;
 
-
+// @RestController annotation in order to simplify the creation of RESTful web services
 @RestController
+// RequestMapping annotation is used to map web requests onto specific handler classes and/or handler methods
 @RequestMapping("/api")
 public class StudentController {
+// Creating the object of StudentService class
+@AutoWired
   private StudentService studentService;
-  
+ // @PostMapping is used to handle POST type of request method
   @PostMapping("/students")
   public ResponseEntity<Student> createStudent(@RequestBody @Valid StudentDTO a){
 	  Student a1=studentService.createStudent(a);
 	  return new ResponseEntity<>(a1,HttpStatus.CREATED);
   }
-  
+  //update Student details
   @PutMapping("/students/{id}")
   public ResponseEntity<Student> updateStudent(@PathVariable("id")int id,@RequestBody StudentDTO studentDTO)
   {
 	  studentService.updateStudent(id, studentDTO);
 	  return ResponseEntity.ok().body(studentService.updateStudent(id, studentDTO));
   }
-  
+  //Getting all Student details
   @GetMapping("/studentname")
   public ResponseEntity<List<Student>> getStudentByStd_Name(@RequestParam(required=false)String name){
 	  List<Student> student=studentService.findByNameContaining(name);
@@ -59,7 +62,7 @@ public class StudentController {
 	  studentService.getAllStudentById(id);
 	  return ResponseEntity.ok().body(studentService.getAllStudentById(id));
   }
-  
+  //delete the Student Details
   @DeleteMapping("/students/{id}")
   public ResponseEntity<Student> deleteStudent(@PathVariable int id){
 	  studentService.deleteStudent(id);
