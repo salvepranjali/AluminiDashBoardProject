317c9473cd89a893942bf13c6a86c9e9ee4be207
salvepranjali <101197017+salvepranjali@users.noreply.github.com>
2023-03-27 03:58:05 -0400

    Update UserNotFoundException.java

--- AluminiDashboardP/src/main/java/com/example/aluminidashboardP/exception/UserNotFoundException.java
+++ AluminiDashboardP/src/main/java/com/example/aluminidashboardP/exception/UserNotFoundException.java
@@ -2,8 +2,10 @@ package com.example.aluminidashboardP.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+//@ResponseStatus to mark a method or an exception class with a status code and reason that should be returned
 @ResponseStatus(HttpStatus.NOT_FOUND)
+//Global Exception Handler class
+{
 public class UserNotFoundException extends RuntimeException {
 	public  UserNotFoundException(String message,Throwable cause)
 	{
